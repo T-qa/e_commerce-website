b8a4723bbb3624b9eb7a29480b7c950a3d6c67b8
Quang Anh <quanganhtrinh1804@gmail.com>
2024-05-29 11:24:01 +0700

    add footer, navigation bar, and admin/user view

---
+++ footer.php
@@ -0,0 +1,68 @@
+<section id="footer">
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col col-lg-3 col-md-6 col-sm-12">
+                    <div class="store-info">
+                        <h3>About the store</h3>
+                        <div class="info">
+                            <ul class="contact-info">
+                                <li>
+                                    <i class="fa-solid fa-location-dot"></i>
+                                    <a href="">Binh Thanh, HCM, ABC Street</a>
+                                </li>
+                                <li>
+                                    <i class="fa-solid fa-phone"></i>
+                                    <a href="tel:+1-541-754-3010
+                      ">+1-541-754-3010
+                                    </a>
+                                </li>
+                                <li>
+                                    <i class="fa-solid fa-envelope"></i>
+                                    <a href="mailto: abc@example.com">abc@example.com</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col col-lg-3 col-md-6 col-sm-12">
+                    <h3>Follow us</h3>
+                    <ul class="social-network-info">
+                        <li>
+                            <a href=""><i class="fa-brands fa-facebook"></i></a>
+                        </li>
+                        <li>
+                            <a href=""><i class="fa-brands fa-instagram"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/T-qa/e_commerce-website"><i class="fa-brands fa-github"></i></a>
+                        </li>
+                        <li>
+                            <a href=""><i class="fa-brands fa-linkedin"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col col-lg-3 col-md-6 col-sm-12">
+                    <h3>Customer support</h3>
+                    <ul>
+                        <li><a href="profile-view.php">My account</a></li>
+                        <li><a href="">Checkout</a></li>
+                        <li><a href="">Cart</a></li>
+                        <li><a href="">FAQ's</a></li>
+                        <li><a href="">Help and support</a></li>
+                    </ul>
+                </div>
+                <div class="col col-lg-3 col-md-6 col-sm-12">
+                    <h3>Site information</h3>
+                    <ul>
+                        <li><a href="">Accessibility</a></li>
+                        <li><a href="">Term and conditions</a></li>
+                        <li><a href="">Privacy notices</a></li>
+                        <li><a href="">Cookie policy</a></li>
+                        <li><a href="">Fraud and scam alert</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</section>
